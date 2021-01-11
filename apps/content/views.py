1559d4c7d39d1704d5812fca9ca8dced1bb923af
theognis1002 <theognis1002@gmail.com>
2021-01-10 21:45:31 -0500

    added redis

--- apps/content/views.py
+++ apps/content/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.cache import cache
 
 
 def test(request):
+    var = cache.get("test")
+    if not var:
+        var = 123
+        cache.set("test", var)
+        print("Setting cache")
+    else:
+        print("Using cache")
     return HttpResponse("<h1>Hello World</h1>")
